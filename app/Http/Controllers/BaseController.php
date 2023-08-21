1f5a2914aaeb7ec64c082df1d287d937d7f129f2
AhmadAlaa-99 <ahmadite99@gmail.com>
2023-08-21 13:02:03 +0200

    gf

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -47,7 +47,6 @@ class BaseController
         }
 
         $deviceToken =DeviceToken::where('email',$user->email)->first();
-         $deviceToken = $deviceToken->fcm_token;
         $SERVER_API_KEY = 'AAAAwjICafo:APA91bGl4kOs_x7Wl9wdFaOkcJSorNZsXdvT_durj5fG6bu21wE6r5_v
         vRY9tHi_NIPhdcNuD4CeIq2F_dlLzTnU-PX9qQCtLGvHbUE9_69jlvoXaIUQ1OczlEeVoFOOv
         B9HqBGDIZVf';
